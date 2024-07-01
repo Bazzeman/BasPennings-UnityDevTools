316178dc1aa1e987849756fe2447f7b3ff31d928
BasPennings <p.freekje@gmail.com>
2024-07-01 02:21:04 +0200

    Improved required attribute
    RequiredAttributePropertyDrawer is now easier to read and understand, RequiredAttribute now has its correct AttributeUsage attribute and the user of the required attribute can now specify a custom description if needed.

--- Assets/Editor/PropertyDrawers/RequiredAttributePropertyDrawer.cs
+++ Assets/Editor/PropertyDrawers/RequiredAttributePropertyDrawer.cs
@@ -1,58 +1,50 @@
-using UnityEditor;
 using UnityEngine;
+using UnityEditor;
 
+/// <summary>
+/// Required attribute property drawer
+/// </summary>
 [CustomPropertyDrawer(typeof(RequiredAttribute))]
 public class RequiredAttributePropertyDrawer : PropertyDrawer
 {
-    readonly Color errorColor = new Color(1, .2f, .2f, .1f);
+    private const float k_helpBoxBottomPadding = 0;
+    private readonly Color k_errorColor = new(1, .2f, .2f, .1f);
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-    {
-        float baseHeight = base.GetPropertyHeight(property, label);
-
-        if (IsFieldEmpty(property))
-        {
-            return baseHeight + EditorGUIUtility.singleLineHeight;
-        }
-
-        return baseHeight;
-    }
+        => IsFieldSupported(property) && IsFieldEmpty(property)
+            ? EditorGUIUtility.singleLineHeight * 3 + k_helpBoxBottomPadding
+            : base.GetPropertyHeight(property, label);
 
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         if (!IsFieldSupported(property))
         {
-            Debug.LogError("Required Attribute placed on incompatible field type");
+            Debug.LogError($"RequiredField is not supported on type of {property.propertyType}!");
+            EditorGUI.PropertyField(position, property, label);
             return;
         }
 
-        Rect basePosition = new Rect(position.x, position.y, position.width, base.GetPropertyHeight(property, label));
-
         if (IsFieldEmpty(property))
         {
-            Rect helpBoxPosition = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight * 3);
-            Rect rectPosition = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight * 3);
-            EditorGUI.HelpBox(helpBoxPosition, "Required", UnityEditor.MessageType.Error);
-            EditorGUI.DrawRect(rectPosition, errorColor);
+            RequiredAttribute requiredFieldAttribute = attribute as RequiredAttribute;
 
-            basePosition.y += EditorGUIUtility.singleLineHeight * 2;
-            EditorGUILayout.Space(EditorGUIUtility.singleLineHeight);
+            EditorGUI.HelpBox(position, requiredFieldAttribute.Description, UnityEditor.MessageType.Error);
+            EditorGUI.DrawRect(position, k_errorColor);
+
+            position = new(
+                position.x, 
+                position.y + EditorGUIUtility.singleLineHeight * 2 + k_helpBoxBottomPadding, 
+                position.width, 
+                EditorGUIUtility.singleLineHeight);
         }
 
-        EditorGUI.PropertyField(basePosition, property, label);
+        EditorGUI.PropertyField(position, property, label);
     }
+
+    private bool IsFieldSupported(SerializedProperty property)
+        => property.propertyType == SerializedPropertyType.String || property.propertyType == SerializedPropertyType.ObjectReference;
 
     private bool IsFieldEmpty(SerializedProperty property)
-    {
-        if (property.propertyType == SerializedPropertyType.ObjectReference && property.objectReferenceValue == null)
-            return true;
-
-        if (property.propertyType == SerializedPropertyType.String && string.IsNullOrEmpty(property.stringValue))
-            return true;
-
-        return false;
-    }
-
-    private bool IsFieldSupported(SerializedProperty property) => 
-        property.propertyType == SerializedPropertyType.ObjectReference || property.propertyType == SerializedPropertyType.String;
+        => property.propertyType == SerializedPropertyType.String && string.IsNullOrEmpty(property.stringValue) 
+            || property.propertyType == SerializedPropertyType.ObjectReference && property.objectReferenceValue == null;
 }
--- Assets/Scripts/Attributes/RequiredAttribute.cs
+++ Assets/Scripts/Attributes/RequiredAttribute.cs
@@ -1,9 +1,11 @@
 using System;
 using UnityEngine;
 
-
 /// <summary>
 /// Required attribute
 /// </summary>
-[AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
-public class RequiredAttribute : PropertyAttribute { }
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+public class RequiredAttribute : PropertyAttribute 
+{ 
+    public string Description { get; set; } = "Required!"; 
+}
